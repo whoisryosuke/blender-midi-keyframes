e9c71cff6726bff2617fa3c0432bf5a6da6240f6
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-04-01 15:05:56 -0700

    :bug: drumpad buttons working

--- docs/src/components/Ryoturia/DrumpadButton.tsx
+++ docs/src/components/Ryoturia/DrumpadButton.tsx
@@ -6,9 +6,13 @@ import { Color, Mesh, MeshPhysicalMaterial } from "three";
 type Props = Partial<JSX.IntrinsicElements["mesh"]> & {};
 
 // This is pulled from the `<Ryoturia />` component, each mesh has a hard-coded position
-const START_Z = 1.453;
+const START_DISTANCE = 1.453;
 const BASE_COLOR = new Color("#FFFFFF");
 const PRESSED_COLOR = new Color("#002EFF");
+// Because floats are inprecise, we can't just say "less than" or "equals"
+// since the object may fluctuate +- 0.001
+// so we check if it's passed a threshold (this buffer)
+const FLOAT_BUFFER = 0.05;
 
 const DrumpadButton = ({ material, debug, ...props }: Props) => {
   const meshRef = useRef<Mesh>(null);
@@ -20,9 +24,11 @@ const DrumpadButton = ({ material, debug, ...props }: Props) => {
       //   meshRef.current.material.uniforms.rotation.value =
       //     meshRef.current.rotation.x;
       material.color =
-        meshRef.current.position.z > START_Z ? PRESSED_COLOR : BASE_COLOR;
+        meshRef.current.position.y < START_DISTANCE - FLOAT_BUFFER
+          ? PRESSED_COLOR
+          : BASE_COLOR;
 
-      //   if (debug) console.log(meshRef.current.position.z);
+      if (debug) console.log(meshRef.current.position);
     }
   });
 

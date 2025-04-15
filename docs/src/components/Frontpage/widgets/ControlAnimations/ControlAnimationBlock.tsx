36da3b6f6956966dc4a06057f1157a2cfb28f6e6
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-04-15 15:30:10 -0700

    :sparkles: control animation block

---
+++ docs/src/components/Frontpage/widgets/ControlAnimations/ControlAnimationBlock.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  BiListOl,
+  BiMenuAltLeft,
+  BiMove,
+  BiCaretRight,
+  BiFile,
+  BiGhost,
+} from "react-icons/bi";
+
+const ICONS = {
+  select_track: BiListOl,
+  select_octave: BiMenuAltLeft,
+  animation_type: BiMove,
+  animation_mode: BiCaretRight,
+  export: BiFile,
+  object: BiGhost,
+};
+
+type Props = {
+  title: string;
+  subtitle: string;
+  icon: keyof typeof ICONS;
+};
+
+const ControlAnimationBlock = ({ title, subtitle, icon }: Props) => {
+  const Icon = ICONS[icon];
+  return (
+    <div className="block">
+      <div className="content">
+        <Icon size={96} />
+        <h3>{title}</h3>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlAnimationBlock;
--- docs/src/components/Frontpage/widgets/ControlAnimations/ControlAnimations.tsx
+++ docs/src/components/Frontpage/widgets/ControlAnimations/ControlAnimations.tsx
@@ -1,21 +1,49 @@
 import React from "react";
+import ControlAnimationBlock from "./ControlAnimationBlock";
+import "./ControlAnimations.css";
 
 type Props = {};
 
 const ControlAnimations = (props: Props) => {
   return (
-    <div className="FrontpageContainer">
+    <div className="FrontpageContainer ControlAnimations">
       <div className="textbox">
         <h2>
-          Control your
+          <span className="highlight">Control</span> your
           <br /> animations
         </h2>
-        <ul>
-          <li>Select MIDI track</li>
-          <li>Select octave (or condense all)</li>
-          <li>Change animation type (move, rotate, or scale)</li>
-          <li>Change animation parameters (distance, axis)</li>
-        </ul>
+        <div className="blocks">
+          <ControlAnimationBlock
+            icon="select_track"
+            title="Select Track"
+            subtitle="Import single or multi-track MIDI files"
+          />
+          <ControlAnimationBlock
+            icon="select_octave"
+            title="Select Octave"
+            subtitle="Isolate to specific octave or condense all of them into one track"
+          />
+          <ControlAnimationBlock
+            icon="animation_type"
+            title="Move, Scale, or Rotate objects"
+            subtitle="For keyframe animations"
+          />
+          <ControlAnimationBlock
+            icon="animation_mode"
+            title="Use Keyframes or Actions"
+            subtitle="Assign action animations for each key"
+          />
+          <ControlAnimationBlock
+            icon="export"
+            title="Export animation to GLTF"
+            subtitle="Import into your favorite 3D engine"
+          />
+          <ControlAnimationBlock
+            icon="object"
+            title="Animate any object"
+            subtitle="Assign any object to any piano key"
+          />
+        </div>
       </div>
 
       <img

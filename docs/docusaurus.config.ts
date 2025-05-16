8890ac49d042588d47166cac1dccaccae519839d
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-05-15 17:35:06 -0700

    :pencil: fix edit link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/whoisryosuke/blender-midi-motion/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
